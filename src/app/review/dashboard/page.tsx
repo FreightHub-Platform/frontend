bb2d7b226150dfe77cf8b9815ae73fd8c3f64e6e
DilkiSewwwandi <2021cs185@stu.ucsc.cmb.ac.lk>
2024-07-25 23:26:26 +0530

    review board

---
+++ src/app/review/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Topbar } from "../../../components/review/topbar/Topbar";
+
+const Review = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Topbar />
+    </div>
+  );
+};
+
+export default Review;
