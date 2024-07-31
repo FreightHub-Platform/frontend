278e08f1c3a6db462016675a05f18940f96accd9
DilkiSewwwandi <2021cs185@stu.ucsc.cmb.ac.lk>
2024-07-31 10:16:22 +0530

    charts-reports

---
+++ src/components/reports/OrderVolumeByMonth.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from "react";
+import MyApexChart from "../charts/MyApexChart";
+const chartData = {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    series: [
+      {
+        name: 'Series 1',
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 150, 200, 300],
+      },
+      {
+        name: 'Series 2',
+        data: [20, 30, 25, 40, 39, 50, 60, 81, 115, 140, 190, 250],
+      },
+    ],  
+}
+
+const OrderVolumeByMonth: React.FC = () => {
+    return (
+        <MyApexChart data={chartData} />
+    );
+  };
+  
+  export default OrderVolumeByMonth;
+  
