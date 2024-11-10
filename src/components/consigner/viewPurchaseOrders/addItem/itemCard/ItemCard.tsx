dd8a7c8410b3e5329c01c3c955124324f5b2c38f
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-11-10 21:55:10 +0530

    Fix item card

--- src/components/consigner/viewPurchaseOrders/addItem/itemCard/ItemCard.tsx
+++ src/components/consigner/viewPurchaseOrders/addItem/itemCard/ItemCard.tsx
@@ -9,11 +9,24 @@ const ItemCard = ({ newItem, cancelItem }) => {
       className="flex flex-row gap-3 overflow-x-auto py-2 mb-1 custom-scrollbar"
     >
       {newItem.map((item, index) => (
-        <div className='flex flex-col pt-3 pb-2 pr-8 pl-3 bg-yellow-200 rounded-lg relative cursor-pointer' key={index} onClick={() => cancelItem(index)}>
-        <CloseIcon className='absolute top-1.5 right-1.5 text-sm'/>
-        <div className='flex'>
-          <p className='font-semibold mr-1 text-xs'>Type:</p>
-          <p className='text-xs'>{item.iTypeId.split(",")[1]}</p>
+        <div
+          className="flex flex-col pt-3 pb-2 pr-8 pl-3 bg-yellow-200 rounded-lg relative cursor-pointer"
+          key={index}
+          onClick={() => cancelItem(index)}
+        >
+          <CloseIcon className="absolute top-1.5 right-1.5 text-sm" />
+          <div className="flex">
+            <p className="font-semibold mr-1 text-xs">Type:</p>
+            <p className="text-xs">{item.iTypeId.split(",")[1]}</p>
+          </div>
+          <div className="flex">
+            <p className="font-semibold mr-1 text-xs">Weight:</p>
+            <p className="text-xs">{item.weight}kg</p>
+          </div>
+          <div className="flex">
+            <p className="font-semibold mr-1 text-xs">CBM:</p>
+            <p className="text-xs">{item.cbm}</p>
+          </div>
         </div>
       ))}
     </Box>
