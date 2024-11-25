3cad7e7a4641b8e2912068d65cad96a9b9a8b6fc
Danujaya00 <danujaya@wso2.com>
2024-11-26 00:09:46 +0530

    Update API base URLs to use localhost for development

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,40 +1,34 @@
-
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://auth.freighthub.danujaya.live/api",
-  // baseURL: "http://localhost:8080/api",
+  // baseURL: "https://auth.freighthub.danujaya.live/api",
+  baseURL: "http://localhost:8080/api",
 });
-
 
 //CONSIGNER APIS
 export const consignerApi = axios.create({
-  baseURL: "https://auth.freighthub.danujaya.live/api/consigner",
-  // baseURL: "http://localhost:8080/api/consigner",
+  // baseURL: "https://auth.freighthub.danujaya.live/api/consigner",
+  baseURL: "http://localhost:8080/api/consigner",
 });
-
 
 //ORDER APIS
 export const orderApi = axios.create({
-  baseURL: "https://auth.freighthub.danujaya.live/api/orders",
-  // baseURL: "http://localhost:8080/api/orders",
+  // baseURL: "https://auth.freighthub.danujaya.live/api/orders",
+  baseURL: "http://localhost:8080/api/orders",
 });
-
 
 //REVIEW APIS backend eka balala hariyata daganna CONTROLLER EKE NAMA 'API/CONTOLLER_NAME'
 export const reviewApi = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "http://localhost:8080/api",
 });
 
 //NOTIFICATION API
 export const notificationApi = axios.create({
-  baseURL: "http://localhost:8080/api"
-})
-
+  baseURL: "http://localhost:8080/api",
+});
 
 //USER APIS
 export const userApi = axios.create({
-  baseURL: "https://auth.freighthub.danujaya.live/api/user",
-  // baseURL: "http://localhost:8080/api/user",
+  // baseURL: "https://auth.freighthub.danujaya.live/api/user",
+  baseURL: "http://localhost:8080/api/user",
 });
-
