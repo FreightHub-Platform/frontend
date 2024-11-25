1412c17d45d18e150adb16554852490b5d77f43f
Danujaya00 <danujaya@wso2.com>
2024-11-25 23:21:23 +0530

    Logout fixes

---
+++ src/app/admin/logout/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import { motion } from "framer-motion";
+
+const LogoutPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    localStorage.removeItem("userToken");
+    Cookies.remove("jwtToken");
+    router.push("/login");
+  }, [router]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div
+        className="
+        flex
+        justify-center
+        items-center
+        w-full
+        h-full
+        bg-gray-100
+        text-gray-700
+        text-lg
+        font-semibold
+      
+      "
+      >
+        <p>Logging out...</p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default LogoutPage;
