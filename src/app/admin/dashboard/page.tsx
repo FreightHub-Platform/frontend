e6fa910fcef76f8866b487b6749d0ac0c215b1c6
danujaya00 <danujaya@wso2.com>
2024-12-02 13:23:05 +0530

    Update apexcharts and react-apexcharts to latest versions

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import VehicleOverview from "../../../components/admin/dashboard/VehicleOverview";
 import IconButton from "@mui/material/IconButton";
 import RefreshIcon from "@mui/icons-material/Refresh";
@@ -11,8 +12,15 @@ import DataInitializer from "../../../components/admin/dashboard/stats/DataIniti
 import DashboardSummaryCards from "../../../components/admin/dashboard/stats/DashboardSummaryCards";
 import VehicleStatusChart from "../../../components/admin/dashboard/VehicleOverviewCard/VehicleStatusChart";
 import OrdersTable from "../../../components/admin/dashboard/stats/OrdersTable";
+import { useState } from "react";
 
 const Admin = () => {
+  const [isDataInitialized, setIsDataInitialized] = useState(false);
+
+  const handleDataInitialized = () => {
+    setIsDataInitialized(true);
+  };
+
   const todayOnTheWay: number = 2;
   const todayUnloading: number = 1;
   const todayLoading: number = 1;
@@ -21,123 +29,129 @@ const Admin = () => {
   return (
     <>
       <AuthGuard>
-        <DataInitializer>
-          <div className=" ">
-            <DashboardSummaryCards />
-          </div>
+        <DataInitializer onInitialized={handleDataInitialized}>
+          {!isDataInitialized ? (
+            <p>Loading dashboard...</p>
+          ) : (
+            <>
+              <div className=" ">
+                <DashboardSummaryCards />
+              </div>
 
-          <div className="flex py-9 px-6 gap-x-8   justify-between">
-            <div className="w-2/4 pr-5 bg-white rounded-md pl-5">
-              <div className="flex mt-4 flex-row justify-between align-middle ">
-                <div className="text-lg">Vehicle Status Overview</div>
-                <div>
-                  <IconButton aria-label="delete" size="small">
-                    <RefreshIcon />
-                  </IconButton>
+              <div className="flex py-9 px-6 gap-x-8   justify-between">
+                <div className="w-2/4 pr-5 bg-white rounded-md pl-5">
+                  <div className="flex mt-4 flex-row justify-between align-middle ">
+                    <div className="text-lg">Vehicle Status Overview</div>
+                    <div>
+                      <IconButton aria-label="delete" size="small">
+                        <RefreshIcon />
+                      </IconButton>
+                    </div>
+                  </div>
+                  <Divider className="mb-4 mt-1" />
+                  <VehicleStatusChart />
+                </div>
+                <div className="w-2/4 pr-5 bg-white rounded-md">
+                  <VehicleOverview />
                 </div>
               </div>
-              <Divider className="mb-4 mt-1" />
-              <VehicleStatusChart />
-            </div>
-            <div className="w-2/4 pr-5 bg-white rounded-md">
-              <VehicleOverview />
-            </div>
-          </div>
 
-          <div className="flex pb-9  px-6 gap-x-8   justify-between">
-            <div className=" w-2/6 pb-6 bg-white rounded-md  px-7">
-              <div className="flex mt-4 flex-row justify-between mb-3 align-middle">
-                <div className="text-lg">Delivery Performance</div>
-                <div>
-                  <IconButton aria-label="delete" size="small">
-                    <RefreshIcon />
-                  </IconButton>
+              <div className="flex pb-9  px-6 gap-x-8   justify-between">
+                <div className=" w-2/6 pb-6 bg-white rounded-md  px-7">
+                  <div className="flex mt-4 flex-row justify-between mb-3 align-middle">
+                    <div className="text-lg">Delivery Performance</div>
+                    <div>
+                      <IconButton aria-label="delete" size="small">
+                        <RefreshIcon />
+                      </IconButton>
+                    </div>
+                  </div>
+                  <Divider className="mb-4 mt-1" />
+
+                  <div className=" flex flex-col gap-y-3">
+                    <DeliveryPerformanceCard
+                      icon="gift"
+                      upordown="up"
+                      amount="299"
+                      text="Packages in transit"
+                      percentage="12%"
+                      bgColor="bg-green-300"
+                    />
+                    <DeliveryPerformanceCard
+                      icon="gift"
+                      upordown="up"
+                      amount="299"
+                      text="Packages in transit"
+                      percentage="12%"
+                      bgColor="bg-green-500"
+                    />
+                    <DeliveryPerformanceCard
+                      icon="gift"
+                      upordown="down"
+                      amount="299"
+                      text="Packages in transit"
+                      percentage="12%"
+                      bgColor="bg-green-600"
+                    />
+                    <DeliveryPerformanceCard
+                      icon="gift"
+                      upordown="up"
+                      amount="299"
+                      text="Packages in transit"
+                      percentage="12%"
+                      bgColor="bg-green-800"
+                    />
+                    <DeliveryPerformanceCard
+                      icon="gift"
+                      upordown="down"
+                      amount="299"
+                      text="Packages in transit"
+                      percentage="12%"
+                      bgColor="bg-green-900"
+                    />
+                  </div>
+                </div>
+                <div className=" w-2/6 bg-white rounded-md p-5">
+                  <div className="flex flex-row justify-between mb-3 align-middle ">
+                    <div className="text-lg">Order Status Distribution</div>
+                    <div></div>
+                  </div>
+                  <Divider className="mb-4 mt-1" />
+                  <div className="flex w-full justify-center content-center h-full align-middle  mx-auto">
+                    <DeliveryExceptions />
+                  </div>
+                </div>
+
+                <div className=" w-2/6 bg-white h-[29rem] rounded-md">
+                  <div className="flex mt-4 flex-row justify-between mb-1 align-middle px-5">
+                    <div className="text-lg">Orders</div>
+                    <div>
+                      <IconButton aria-label="delete" size="small">
+                        <RefreshIcon />
+                      </IconButton>
+                    </div>
+                  </div>
+                  <Divider className=" mt-2 px-3 mx-5" />
+                  <OrdersTabs />
                 </div>
               </div>
-              <Divider className="mb-4 mt-1" />
 
-              <div className=" flex flex-col gap-y-3">
-                <DeliveryPerformanceCard
-                  icon="gift"
-                  upordown="up"
-                  amount="299"
-                  text="Packages in transit"
-                  percentage="12%"
-                  bgColor="bg-green-300"
-                />
-                <DeliveryPerformanceCard
-                  icon="gift"
-                  upordown="up"
-                  amount="299"
-                  text="Packages in transit"
-                  percentage="12%"
-                  bgColor="bg-green-500"
-                />
-                <DeliveryPerformanceCard
-                  icon="gift"
-                  upordown="down"
-                  amount="299"
-                  text="Packages in transit"
-                  percentage="12%"
-                  bgColor="bg-green-600"
-                />
-                <DeliveryPerformanceCard
-                  icon="gift"
-                  upordown="up"
-                  amount="299"
-                  text="Packages in transit"
-                  percentage="12%"
-                  bgColor="bg-green-800"
-                />
-                <DeliveryPerformanceCard
-                  icon="gift"
-                  upordown="down"
-                  amount="299"
-                  text="Packages in transit"
-                  percentage="12%"
-                  bgColor="bg-green-900"
-                />
-              </div>
-            </div>
-            <div className=" w-2/6 bg-white rounded-md p-5">
-              <div className="flex flex-row justify-between mb-3 align-middle ">
-                <div className="text-lg">Order Status Distribution</div>
-                <div></div>
-              </div>
-              <Divider className="mb-4 mt-1" />
-              <div className="flex w-full justify-center content-center h-full align-middle  mx-auto">
-                <DeliveryExceptions />
-              </div>
-            </div>
-
-            <div className=" w-2/6 bg-white h-[29rem] rounded-md">
-              <div className="flex mt-4 flex-row justify-between mb-1 align-middle px-5">
-                <div className="text-lg">Orders</div>
-                <div>
-                  <IconButton aria-label="delete" size="small">
-                    <RefreshIcon />
-                  </IconButton>
+              <div className="flex pb-9 px-6 gap-x-8   justify-between">
+                <div className="w-full flex flex-col p-5 bg-white rounded-md">
+                  <div className="flex flex-row justify-between mb-2 align-middle ">
+                    <div className="text-lg">Order Details</div>
+                    <div>
+                      <IconButton aria-label="delete" size="small">
+                        <RefreshIcon />
+                      </IconButton>
+                    </div>
+                  </div>
+                  <Divider className="mb-4 mt-1" />
+                  <OrdersTable />
                 </div>
               </div>
-              <Divider className=" mt-2 px-3 mx-5" />
-              <OrdersTabs />
-            </div>
-          </div>
-
-          <div className="flex pb-9 px-6 gap-x-8   justify-between">
-            <div className="w-full flex flex-col p-5 bg-white rounded-md">
-              <div className="flex flex-row justify-between mb-2 align-middle ">
-                <div className="text-lg">Order Details</div>
-                <div>
-                  <IconButton aria-label="delete" size="small">
-                    <RefreshIcon />
-                  </IconButton>
-                </div>
-              </div>
-              <Divider className="mb-4 mt-1" />
-              <OrdersTable />
-            </div>
-          </div>
+            </>
+          )}
         </DataInitializer>
       </AuthGuard>
     </>
